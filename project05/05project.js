0317fc14a92335eb2bced2f3df4918151cdf6ff3
zerocuse <zmgrande@syr.edu>
2024-04-07 16:13:57 -0400

    project05

---
+++ project05/05project.js
@@ -0,0 +1,64 @@
+function showInfo1() {
+    var x = document.getElementById("info1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo2() {
+    var x = document.getElementById("info2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo3() {
+    var x = document.getElementById("info3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo4() {
+    var x = document.getElementById("info4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo5() {
+    var x = document.getElementById("info5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo6() {
+    var x = document.getElementById("info6");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo7() {
+    var x = document.getElementById("info7");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function showInfo8() {
+    var x = document.getElementById("info8");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
