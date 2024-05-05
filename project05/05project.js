7caf7a1c6d7488566c166e379ddab129be77a059
zerocuse <zmgrande@syr.edu>
2024-05-04 22:57:45 -0400

    updating website again

--- project05/05project.js
+++ project05/05project.js
@@ -1,8 +1,15 @@
-function showInfo(id) {
+function showInfo(id, more) {
     var x = document.getElementById(id);
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
+    }
+
+    var y = document.getElementById(more);
+    if (x.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
     }
 }
